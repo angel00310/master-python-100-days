f6ad3603b5de170c9af8fd2377297b55ee4ef157
angel00310 <A8303103@gmail.com>
2024-07-13 22:56:40 +0800

    day4 test

---
+++ DAY23/car_manager.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    #製造隨機的車
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+        #創造1/6的機會才製造車
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            #在-250~250間隨機定義Y軸
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    #讓車往左移動
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+            #因為頭朝東，所以後退變成往左移動
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
